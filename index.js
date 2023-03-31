a7d8a198858075757fb2ef43bf3b12c541727a79
Michael Tanaka <tanakam4214@gmail.com>
2023-03-30 19:29:08 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const arrayOfDriverObjs = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(arr,string){
+   const match = arr.filter(function(name){
+    if(name === string || name.toLowerCase() === string.toLowerCase() || name.toUpperCase() === string.toUpperCase()){
+        return string
+    }
+   })
+   return match
+}
+findMatching(drivers,'BOBBY');
+console.log(findMatching(drivers,'BOBBY'));
+
+function fuzzyMatch(arr,string){
+    const letterMatch = arr.filter(function(name){
+        if (name.startsWith(string))
+        return name
+    })
+    return letterMatch
+}
+fuzzyMatch(drivers,'Bo')
+console.log(fuzzyMatch(drivers,'Bo'))
+
+function matchName(arr,string){
+    const obj = arr.filter(function(key){
+        if(string === key.name){
+            return key.name
+        }
+    })
+    return obj
+}
+
+matchName(arrayOfDriverObjs,"Bobby")
+console.log(matchName(arrayOfDriverObjs,'Bobby'))
